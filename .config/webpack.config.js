af73a0aed1f0276a199f3fae210b2ba2ced8ba6e
Matt Milburn <me@mattmilburn.com>
2020-08-02 14:16:06 -0500

    Add webpack config

---
+++ .config/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require( 'path' );
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve( process.cwd(), 'src/index.js' ),
+  output: {
+    path: path.resolve( process.cwd(), 'dist' ),
+    filename: 'ness.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: __dirname + '/.config/postcss.config.js'
+              }
+            }
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin( {
+      filename: 'ness.bundle.css',
+    } ),
+  ],
+};
